86f01db6040212f14c6e2e8e0a168aeec14bc19e
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-17 12:08:24 -0400

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      <div className='font-semibold text-14/16'>SKINSTRIC <span className='text-[#1A1B1C70]'>[ INTRO ]</span></div>
-      <div>ENTER CODE</div>
+    <div className='flex justify-between px-16 py-11 items-center'>
+      <div className="flex font-semibold text-sm">
+        <div className=''>SKINSTRIC</div>
+        <div className='text-[#1A1B1C70] px-4'>[ INTRO ]</div>
+      </div>
+      <div className='py-4 px-8 font-semibold bg-[#1A1B1C] text-[#FCFCFC] text-[10px]'>ENTER CODE</div>
     </div>
   )
 }
