f4e9dc618560b73786016442bb6363e379f36264
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-17 12:37:01 -0400

    partial frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-[71vh] relative">
+      <div className="h-[300px] w-[300px] border-2 border-[#A0A4AB] border-dotted absolute rotate-45 left-0"></div>
+      <div className="font-light text-[128px] leading-[120px] text-center text-[#1A1B1C]">Sophisticated skincare</div>
+      <div className="h-[300px] w-[300px] border-2 border-[#A0A4AB] border-dotted absolute rotate-45 right-0"></div>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='flex justify-between px-16 py-11 items-center'>
-      <div className="flex font-semibold text-sm">
+      <div className="flex font-semibold text-sm leading-[16px]">
         <div className=''>SKINSTRIC</div>
         <div className='text-[#1A1B1C70] px-4'>[ INTRO ]</div>
       </div>
-      <div className='py-4 px-8 font-semibold bg-[#1A1B1C] text-[#FCFCFC] text-[10px]'>ENTER CODE</div>
+      <div className='py-4 px-8 leading-[16px] font-semibold bg-[#1A1B1C] text-[#FCFCFC] text-[10px]'>ENTER CODE</div>
     </div>
   )
 }
