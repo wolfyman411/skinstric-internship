b93eac8e15d01d17a3d21fd8837bcdf96921bbde
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-17 11:48:55 -0400

    partial frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <div className='font-semibold text-14/16'>SKINSTRIC <span className='text-[#1A1B1C70]'>[ INTRO ]</span></div>
+      <div>ENTER CODE</div>
+    </div>
+  )
+}
