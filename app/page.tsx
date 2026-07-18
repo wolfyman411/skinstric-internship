bd009ce81ad9900de015d1d2ab6e161f59df2f30
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-18 13:07:36 -0400

    partial animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,46 @@
-import Image from "next/image"
-import button from "../public/assets/buttin-icon-shrunk.svg"
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import button from "../public/assets/buttin-icon-shrunk.svg";
 
 export default function Home() {
+  const [hoverState, setHoverState] = useState("");
+
   return (
     <section id="home" className="container">
-      <div className="home-rectangle home-rectangle--left"></div>
-      <div className="home-rectangle home-rectangle--right"></div>
+      <div className={`home-rectangle home-rectangle--left ${hoverState === "right" ? "hidden" : ""}`}></div>
+      <div className={`home-rectangle home-rectangle--right ${hoverState === "left" ? "hidden" : ""}`}></div>
       <div className="row">
-        <div className="home-button">
+        <div className={`home-button ${hoverState === "right" ? "hidden" : ""}`}>
           <div className="home-button--rectangle"></div>
-          <div className="home-button--wrapper">
-            <Image src={button} alt="left"/>
+          <div
+            className="home-button--wrapper"
+            onMouseEnter={() => setHoverState("left")}
+            onMouseLeave={() => setHoverState("")}
+          >
+            <Image src={button} alt="left" />
             <div className="home-button--text">DISCOVER A.I.</div>
           </div>
         </div>
-        <h1>Sophisticated skincare</h1>
-        <div className="home-button">
+        <div className={`home-title ${hoverState !== "" ? `home-title--${hoverState}` : ""}`}>
+          Sophisticated skincare
+        </div>
+        <div className={`home-button ${hoverState === "left" ? "hidden" : ""}`}>
           <div className="home-button--rectangle"></div>
-          <div className="home-button--wrapper">
+          <div
+            className="home-button--wrapper"
+            onMouseEnter={() => setHoverState("right")}
+            onMouseLeave={() => setHoverState("")}
+          >
             <div className="home-button--text">TAKE TEST</div>
-            <Image src={button} alt="right" style={{rotate:"180deg"}}/>
+            <Image src={button} alt="right" style={{ rotate: "180deg" }} />
           </div>
         </div>
       </div>
       <div className="home-info">
-        Skinstric developed an A.I. that creates 
-        a highly-personalised routine tailored to 
+        Skinstric developed an A.I. that creates
+        a highly-personalised routine tailored to
         what your skin needs.
       </div>
     </section>
