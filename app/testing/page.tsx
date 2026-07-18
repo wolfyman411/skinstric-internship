a83ec12dbd4884374a4211fe825d860718d42406
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-18 15:48:50 -0400

    basic testing page css and html done

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -2,15 +2,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import button from "../../public/assets/buttin-icon-shrunk.svg";
+import "./testing.css"
 
 export default function page() {
   return (
-    <section id="testing">
-      <div className="container">
+    <section id="testing" className="container">
         <div className="row">
             <div className="testing-analysis">to start analysis</div>
             <div className="testing-form">
-
+                <div className="testing-rectangle"></div>
+                <div className="testing-rectangle"></div>
+                <div className="testing-rectangle"></div>
+                <div className="testing-subtitle">click to type</div>
+                <input type="text" className="testing-input" placeholder='Introduce Yourself'/>
             </div>
             <div className="testing-back">
                 <Link href="/" className="home-button--wrapper">
@@ -19,7 +23,6 @@ export default function page() {
                 </Link>
             </div>
         </div>
-      </div>
     </section>
   )
 }
