4540db8f9fbc140d4dc5f37aee58a348c4b8a4e6
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-07-23 11:03:04 -0400

    summary css refactor

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react'
-import './summary.css'
+import styles from './summary.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import button from "../../public/assets/buttin-icon-shrunk.svg";
@@ -76,13 +76,13 @@ export default function page() {
   }
 
   function changeDisplay(e:Element,type:string) {
-    const tabs = document.querySelectorAll(".chart-tab")
+    const tabs = document.querySelectorAll(`.${styles["chart-tab"]}`)
 
     for (const tab of tabs) {
-        tab.classList.remove("chart-tab--active")
+        tab.classList.remove(styles["chart-tab--active"])
     }
 
-    e.classList.add("chart-tab--active")
+    e.classList.add(styles["chart-tab--active"])
 
     if (type === "race") {
         setSelectedItem(getRace)
@@ -99,13 +99,13 @@ export default function page() {
 
   function changeSelectedItem(e:Element,info:[string,number]) {
 
-    const options = document.querySelectorAll(".chart-option")
+    const options = document.querySelectorAll(`.${styles["chart-option"]}`)
 
     for (const option of options) {
-        option.classList.remove("chart-option--active")
+        option.classList.remove(styles["chart-option--active"])
     }
 
-    e.classList.add("chart-option--active")
+    e.classList.add(styles["chart-option--active"])
 
     if (displayCategory === "race") {
         setRace(info)
@@ -124,63 +124,63 @@ export default function page() {
   function itemHTML([category, value]: [string, number],index:number) {
 
     return (
-        <div className={`chart-option ${category === selectedItem[0] ? "chart-option--active" : ""}`} key={index} onClick={(e) => changeSelectedItem(e.currentTarget,[category,value])}>
-            <div className="chart-option--left">
-                <Image src={radioButtionChecked} alt='radioButton' className="chart-option--icon--checked"></Image>
-                <Image src={radioButtion} alt='radioButton' className="chart-option--icon"></Image>
-                <div className="chart-option--name">{category}</div>
+        <div className={`${styles["chart-option"]} ${category === selectedItem[0] ? styles["chart-option--active"] : ""}`} key={index} onClick={(e) => changeSelectedItem(e.currentTarget,[category,value])}>
+            <div className={styles["chart-option--left"]}>
+                <Image src={radioButtionChecked} alt='radioButton' className={styles["chart-option--icon--checked"]}></Image>
+                <Image src={radioButtion} alt='radioButton' className={styles["chart-option--icon"]}></Image>
+                <div className={styles["chart-option--name"]}>{category}</div>
             </div>
-            <div className="chart-option--right">{Math.floor(value*100)} %</div>
+            <div className={styles["chart-option--right"]}>{Math.floor(value*100)} %</div>
         </div>
     )
   }
 
   return (
-    <section id="summary" className="container">
+    <section className={`${styles.summary} container`}>
         <div className="row">
-            <div className="summary-analysis">
-                <div className="summary-analysis--analysis">a.i. analysis</div>
-                <div className="summary-analysis--title">demographics</div>
-                <div className="summary-analysis--subtitle">predicted race & age</div>
+            <div className={styles["summary-analysis"]}>
+                <div className={styles["summary-analysis--analysis"]}>a.i. analysis</div>
+                <div className={styles["summary-analysis--title"]}>demographics</div>
+                <div className={styles["summary-analysis--subtitle"]}>predicted race & age</div>
             </div>
-            <div className="analysis-chart--wrapper">
-                <div className="chart-tabs">
-                    <div className="chart-tab chart-tab--active" onClick={(e) => changeDisplay(e.currentTarget,"race")}>
-                        <div className="chart-tab--line"></div>
-                        <div className="chart-tab--selection">{getRace[0]}</div>
-                        <div className="chart-tab--title">race</div>
+            <div className={styles["analysis-chart--wrapper"]}>
+                <div className={styles["chart-tabs"]}>
+                    <div className={`${styles["chart-tab"]} ${styles["chart-tab--active"]}`} onClick={(e) => changeDisplay(e.currentTarget,"race")}>
+                        <div className={styles["chart-tab--line"]}></div>
+                        <div className={styles["chart-tab--selection"]}>{getRace[0]}</div>
+                        <div className={styles["chart-tab--title"]}>race</div>
                     </div>
-                    <div className="chart-tab" onClick={(e) => changeDisplay(e.currentTarget,"age")}>
-                        <div className="chart-tab--line"></div>
-                        <div className="chart-tab--selection">{getAge[0]}</div>
-                        <div className="chart-tab--title">age</div>
+                    <div className={styles["chart-tab"]} onClick={(e) => changeDisplay(e.currentTarget,"age")}>
+                        <div className={styles["chart-tab--line"]}></div>
+                        <div className={styles["chart-tab--selection"]}>{getAge[0]}</div>
+                        <div className={styles["chart-tab--title"]}>age</div>
                     </div>
-                    <div className="chart-tab" onClick={(e) => changeDisplay(e.currentTarget,"sex")}>
-                        <div className="chart-tab--line"></div>
-                        <div className="chart-tab--selection">{getSex[0]}</div>
-                        <div className="chart-tab--title">sex</div>
+                    <div className={styles["chart-tab"]} onClick={(e) => changeDisplay(e.currentTarget,"sex")}>
+                        <div className={styles["chart-tab--line"]}></div>
+                        <div className={styles["chart-tab--selection"]}>{getSex[0]}</div>
+                        <div className={styles["chart-tab--title"]}>sex</div>
                     </div>
                 </div>
-                <div className="chart-display">
-                    <div className="chart-tab--line"></div>
-                    <div className='chart-display--wrapper'>
-                        <div className="chart-display--title">{selectedItem[0]}</div>
-                        <div className="chart-display--graph--wrapper">
-                            <canvas ref={chartRef} className="chart-display--graph" />
-                            <div className="chart-display--graph-text">
+                <div className={styles["chart-display"]}>
+                    <div className={styles["chart-tab--line"]}></div>
+                    <div className={styles["chart-display--wrapper"]}>
+                        <div className={styles["chart-display--title"]}>{selectedItem[0]}</div>
+                        <div className={styles["chart-display--graph--wrapper"]}>
+                            <canvas ref={chartRef} className={styles["chart-display--graph"]} />
+                            <div className={styles["chart-display--graph-text"]}>
                                 <div>{Math.floor(selectedItem[1] * 100)}</div>
-                                <div className="chart-display--graph-percentage">%</div>
+                                <div className={styles["chart-display--graph-percentage"]}>%</div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="chart-options--wrapper">
-                    <div className="chart-tab--line"></div>
-                    <div className="chart-options--top">
-                        <div className="chart-options--top-item">Race</div>
-                        <div className="chart-options--top-item">A.I. confidence</div>
+                <div className={styles["chart-options--wrapper"]}>
+                    <div className={styles["chart-tab--line"]}></div>
+                    <div className={styles["chart-options--top"]}>
+                        <div className={styles["chart-options--top-item"]}>Race</div>
+                        <div className={styles["chart-options--top-item"]}>A.I. confidence</div>
                     </div>
-                    <div className="chart-options">
+                    <div className={styles["chart-options"]}>
                         {
                             displayItems.map((item,index) => (
                                 itemHTML(item,index)
@@ -189,17 +189,17 @@ export default function page() {
                     </div>
                 </div>
             </div>
-            <div className="summary-footer">
+            <div className={styles["summary-footer"]}>
                 <div className="testing-back">
                     <Link href="/select" className="home-button--wrapper">
                         <Image src={button} alt="back"/>
                         <div className="back-button--text">BACK</div>
                     </Link>
                 </div>
-                <div className="disclaimer">If A.I. estimate is wrong, select the correct one.</div>
-                <div className="footer-buttons">
-                    <div className="footer-button">reset</div>
-                    <Link href={"/select"} className="footer-button">confirm</Link>
+                <div className={styles.disclaimer}>If A.I. estimate is wrong, select the correct one.</div>
+                <div className={styles["footer-buttons"]}>
+                    <div className={styles["footer-button"]}>reset</div>
+                    <Link href={"/select"} className={styles["footer-button"]}>confirm</Link>
                 </div>
             </div>
         </div>
